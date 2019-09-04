8d2ea4b6c62da5368ad066a30f5029da1d640ddb
Andres Ramirez Quiros <andrew.rq+github@gmail.com>
2019-09-04 11:26:48 -0600

    Global variables to modify the size of cache/memory

--- src/main.go
+++ src/main.go
@@ -4,10 +4,15 @@ import (
 	"fmt"
 )
 
+//PowerOff - bool to keep main alive or kill every thread if true
+var PowerOff = false 
+//CellsInMemory - int that has the amount of memory lines in the simulated program
+const CellsInMemory = 16
+//CellsInCache - int that has the amount of cache lines in the simulated program
+const CellsInCache = 8
+
 func main() {
 	fmt.Println("Hello World. How are you today?")
 	fmt.Println("")
 
-	cache := NewCache()
-	fmt.Println(cache)
 }
